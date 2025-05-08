e71df6e84e0e926fdb68f22ef772addc94a51566
Bowen Yang <boweny920@gmail.com>
2025-05-08 10:47:25 -0500

    security_update

--- Bioinformatics_APP/forms.py
+++ Bioinformatics_APP/forms.py
@@ -13,66 +13,48 @@ LABS = [(line.strip(), line.strip()) for line in open(os.path.join(settings.BASE
 class PublicDataForm(forms.Form):
     
     SRAID = forms.CharField(max_length=2000, required=True, 
-                            widget=forms.Textarea(attrs={'placeholder': 'Enter Sample SRA ID here', 'rows': 1, 'cols': 25})
+                            widget=forms.Textarea(attrs={'placeholder': 'Enter Sample SRA ID here, one per line', 'rows': 5, 'cols': 25})
                             )
-    SampleName = forms.CharField(max_length=2000, required=True,
-                                 widget=forms.Textarea(attrs={'placeholder': 'Enter Sample Name here', 'rows': 1, 'cols': 25})
-                                 )
-    ReadLength = forms.CharField(max_length=10, required=True,
-                                    widget=forms.Textarea(attrs={"placeholder": "Enter Read Length", "rows": 1, "cols": 15})
-                                    )
-    Description = forms.CharField(required=False,
-        widget=forms.Textarea(attrs={'placeholder': 'Put a short description of your sample. Leave empty if None', 'rows': 1, 'cols': 55})
-        )
+    
+    UserID = forms.CharField(max_length=10, required=True,
+                             widget=forms.Textarea(attrs={'placeholder': 'Enter Stowers User ID here', 'rows': 1, 'cols': 25})
+                             )
+    
+    Lab = forms.ChoiceField(choices=LABS, 
+                            widget=forms.Select(attrs={'placeholder': 'Choose Your lab', 'rows': 1, 'cols': 15})
+    )
+    
+    Reference = forms.ChoiceField(choices=GENOMES, 
+                                  widget=forms.Select(attrs={'placeholder': 'Choose One Genome Per Submission', 'rows': 1, 'cols': 15})
+                                  )
     
     def clean_SRAID(self):
         data = self.cleaned_data['SRAID']
+        # Consider security issues with the input
+        if "/bin/bash" in data or "/bin/sh" in data:
+            raise forms.ValidationError("Invalid input detected in SRAID. Please check your input.")
+        if "rm" in data or "mv" in data:
+            raise forms.ValidationError("Invalid input detected in SRAID. Please check your input.")
+
         # Make sure you have SRA IDs in the input 
         if "SRA" not in data.upper():
             raise forms.ValidationError("SRAID field must contain 'SRA'.")
         ## Need to consider the table to NOT have two SRA ids in the same table!
-        SRA_list = [item.strip() for item in data.split('|') if item.strip()]
-        seen = set()
-        duplicates = set(x for x in SRA_list if x in seen or seen.add(x))
-
-        if len(duplicates) > 0:
-            raise forms.ValidationError(f"Check IDs {" ".join(duplicates)} must be unique.")
+        if any(item.rstrip() for item in data.split('\n') if " " in item.rstrip()):
+            raise forms.ValidationError("SRAID field must not contain spaces.")
+        
         
         """Splits SRAID by '|' into a list"""
-        return [item.strip() for item in data.split('|') if item.strip()]  # Remove empty values
+        return set(item.strip() for item in data.split('\n'))  # Remove empty values
 
-    def clean_SampleName(self):
-        data = self.cleaned_data['SampleName']
-        """Splits SampleName by '|' into a list"""
-
-        return [item.strip() for item in data.split('|') if item.strip()]
-    
-    def clean_ReadLength(self):
-        data = self.cleaned_data["ReadLength"]
-        if any(int(v.strip()) for v in data.split("|") if int(v.strip()) <= 0):
-            raise forms.ValidationError("Read length must be a positive integer")
-        else:
-            return [v.strip() for v in data.split("|") if v.strip()]
+    def clean_UserID(self):
+        data = self.cleaned_data['UserID']
+        if "/bin/bash" in data or "/bin/sh" in data:
+            raise forms.ValidationError("Invalid input detected in UserID. Please check your input.")
+        if "rm" in data or "mv" in data:
+            raise forms.ValidationError("Invalid input detected in UserID. Please check your input.")
         
-    def clean_Description(self):
-        data = self.cleaned_data['Description']
-        """Splits Description by '|' into a list"""
-        return [item.strip() for item in data.split('|') if item.strip()]
+        return data
 
 
-class User_Info_Form(forms.Form):
-    UserID = forms.CharField(max_length=100, required=True,
-                             widget=forms.Textarea(attrs={'placeholder': 'Enter Stowers User ID here', 'rows': 1, 'cols': 25})
-                             )
-    Lab = forms.ChoiceField(choices=LABS, 
-                            widget=forms.Select(attrs={'placeholder': 'Choose Your lab', 'rows': 1, 'cols': 15})
-    )
-    PROJECT_NAME = forms.CharField(max_length=100, required=True,
-                                   widget=forms.Textarea(attrs={'placeholder': 'Enter Project Name here', 'rows': 1, 'cols': 25})
-                                   )
-    ReadType = forms.ChoiceField(choices=[('PE', 'Paired-End'), ('SE', 'Single-End')],
-                                 widget=forms.Select(attrs={'placeholder': 'Choose One Read Type Per Submission', 'rows': 1, 'cols': 15})
-                                 )
-    Reference = forms.ChoiceField(choices=GENOMES, 
-                                  widget=forms.Select(attrs={'placeholder': 'Choose One Genome Per Submission', 'rows': 1, 'cols': 15})
-                                  )
+    
--- Bioinformatics_APP/views.py
+++ Bioinformatics_APP/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import os,time
 from django.http import HttpResponse
 from django.utils import timezone
-from .forms import PublicDataForm, User_Info_Form
+from .forms import PublicDataForm
 from django.http import HttpResponseRedirect
 from django.conf import settings
 from .utils import pubdata
@@ -16,56 +16,29 @@ def publicdata(request):
     if request.method == "POST":
         data = request.POST # This is not validated data. The below function validates the data, but only takes the last value of each field. 
 
-        userData = {"Lab": data.get("Lab"), 
-                    "UserID": data.get("UserID"), 
-                    "PROJECT_NAME": data.get("PROJECT_NAME"),
-                    "ReadType": data.get("ReadType"),
-                    "Reference": data.get("Reference")
-                    }
-        
-        userInfo = User_Info_Form(userData)
-        if not userInfo.is_valid():
-            print(userInfo.errors)
-            return render(request, "Bioinformatics/publicData_submitted_formatError.html", context = {"form": userInfo})
-
-        sraid_values = data.getlist("SRAID")
-        samplename_values = data.getlist("SampleName")
-        read_length_values = data.getlist("ReadLength")
-        description_values = data.getlist("Description")
-        # Join the values into a string using "|"
-        sraid_values_concate = "|".join(sraid_values)
-        samplename_values_concate = "|".join(samplename_values)
-        read_length_values_concate = "|".join(read_length_values)
-        description_values_concate = "|".join(description_values)
-        data = data.copy() #beacuse QueryDict is immutable
-        data["SRAID"] = sraid_values_concate
-        data["SampleName"] = samplename_values_concate
-        data["ReadLength"] = read_length_values_concate
-        data["Description"] = description_values_concate
-        print(data)
         form = PublicDataForm(data)
         if form.is_valid():
-            sraid = form.cleaned_data["SRAID"]
-            samplename = form.cleaned_data["SampleName"]
-            readLength = form.cleaned_data["ReadLength"]
-            description = form.cleaned_data["Description"]
+            sraid = form.cleaned_data['SRAID']
+            Lab=userInfo.cleaned_data["Lab"],
+            UserID=userInfo.cleaned_data["UserID"]
+            print(sraid, Lab, UserID)
 
             # Using utils to create the xlsx file
-            pubdata_obj = pubdata(
-                Lab=userInfo.cleaned_data["Lab"],
-                UserID=userInfo.cleaned_data["UserID"],
-                PROJECT_NAME=userInfo.cleaned_data["PROJECT_NAME"],
-                ReadType=userInfo.cleaned_data["ReadType"],
-                Reference=userInfo.cleaned_data["Reference"],
-                SRAID=sraid,
-                SampleName=samplename,
-                ReadLength=readLength,
-                Description=description,
-            )
+            # pubdata_obj = pubdata(
+            #     Lab=userInfo.cleaned_data["Lab"],
+            #     UserID=userInfo.cleaned_data["UserID"],
+            #     PROJECT_NAME=userInfo.cleaned_data["PROJECT_NAME"],
+            #     ReadType=userInfo.cleaned_data["ReadType"],
+            #     Reference=userInfo.cleaned_data["Reference"],
+            #     SRAID=sraid,
+            #     SampleName=samplename,
+            #     ReadLength=readLength,
+            #     Description=description,
+            # )
             
-            xlsx_table = pubdata_obj.public_xlsx_maker()
-            # pubdata_obj.run_nextflow(xlsx_path)
-            pubdata_obj.script_nextflow(xlsx_table)
+            # xlsx_table = pubdata_obj.public_xlsx_maker()
+            # # pubdata_obj.run_nextflow(xlsx_path)
+            # pubdata_obj.script_nextflow(xlsx_table)
 
             return render(request,"Bioinformatics/publicData_submitted.html")
         else:
@@ -74,8 +47,7 @@ def publicdata(request):
 
     else:
         form = PublicDataForm()
-        userInfo = User_Info_Form()
-    context = {"form": form, "userInfo": userInfo}
+    context = { "form": form }
     
     return render(request, "Bioinformatics/publicData_form.html", context=context)
 
